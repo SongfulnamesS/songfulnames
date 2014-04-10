cf78a7b6ab885ee003c52ac29325d9ea95fd235a
ISongfulnamesSI@gmail.com <ISongfulnamesSI@gmail.com@56615b9d-a27f-5bd6-95a2-5bceebee896c>
2014-04-10 16:24:24 +0000

    ff

--- estilos.js
+++ estilos.js
@@ -4,7 +4,10 @@ showRandomImg = true;
 aBold = true;
 summaryPost = 200;
 summaryTitle = 20;
+label1 = "Capitulo";
 numposts2 = 99999;
+numposts1 = 15;
+
 
 function removeHtmlTag(strx, chop) {
     var s = strx.split("<");
@@ -118,7 +121,7 @@ function capanime(json) {
             }
         }
         var daystr = day + ' ' + m + ' ' + y;
-      var trtd = '<li><a class="rated_avatar" title="'+ posttitle +'" href="' + posturl + '"><img src="' + img[i] + '" width="47" height="47" border="0" alt="' + posttitle + '"/></a><a class="rated_title" href="' + posturl + '">' + posttitle + '</a><div class="rated_stars"><span>'+daystr+'</span><img src="http://i.imgur.com/GWOKAA9.png" style="height:11px; vertical-align:middle;" title="Progamacion Jkanime" alt="Progamacion Jkanime"></div><a class="rated_more" href=' + posturl + '><img style="vertical-align:middle;" src="http://i.imgur.com/VBwAaDQ.png" alt="Ver Anime"></a></li>';
+      var trtd = '<li><a class="rated_avatar" title="'+ posttitle +'" href="' + posturl + '"><img src="' + img[i] + '" width="47" height="47" border="0" alt="' + posttitle + '"/></a><a class="rated_title" href="' + posturl + '">' + posttitle + '</a><div class="rated_stars"><span>'+daystr+'</span><img src="http://4.bp.blogspot.com/-iLWPFywYVgg/U0a2zVfFqeI/AAAAAAAACA0/wEp4CmjnBLc/s1600/estrella.png" style="height:11px; vertical-align:middle;" title="Progamacion Jkanime" alt="Progamacion Jkanime"></div><a class="rated_more" href=' + posturl + '><img style="vertical-align:middle;" src="http://3.bp.blogspot.com/-Tb6i2OrgUrs/U0a21laJahI/AAAAAAAACA8/21CmqxspRYw/s1600/verr.png" alt="Ver Anime"></a></li>';
         document.write(trtd);
         j++
     }
@@ -443,4 +446,59 @@ numero = 0;
       document.write(trtd);
         j++
     }
+}
+function showrecentposts(json) {
+j = (showRandomImg) ? Math.floor((imgr.length+1)*Math.random()) : 0;
+img = new Array();
+document.write('<ul>');
+for (var i = 0; i < numposts1; i++) {
+var entry = json.feed.entry[i];
+var posttitle = entry.title.$t;
+var pcm;
+var posturl;
+if (i == json.feed.entry.length) break;
+for (var k = 0; k < entry.link.length; k++) {
+if (entry.link[k].rel == 'alternate') {
+posturl = entry.link[k].href;
+break;
+}
+}
+
+for (var k = 0; k < entry.link.length; k++) {
+if (entry.link[k].rel == 'replies' && entry.link[k].type == 'text/html') {
+pcm = entry.link[k].title.split(" ")[0];
+break;
+}
+}
+
+if ("content" in entry) {
+var postcontent = entry.content.$t;}
+else
+if ("summary" in entry) {
+var postcontent = entry.summary.$t;}
+else var postcontent = "";
+postdate = entry.published.$t;
+if(j>imgr.length-1) j=0;
+img[i] = imgr[j];
+s = postcontent ; a = s.indexOf("<img"); b = s.indexOf("src=\"",a); c = s.indexOf("\"",b+5); d = s.substr(b+5,c-b-5);
+if((a!=-1)&&(b!=-1)&&(c!=-1)&&(d!="")) img[i] = d;
+//cmtext = (text != 'no') ? '<i><font color="'+acolor+'">('+pcm+' '+text+')</font></i>' : '';
+var month = [1,2,3,4,5,6,7,8,9,10,11,12];
+var month2 = ["Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"];
+var day = postdate.split("-")[2].substring(0,2);
+var m = postdate.split("-")[1];
+var y = postdate.split("-")[0];
+for(var u2=0;u2<month.length;u2++){
+if(parseInt(m)==month[u2]) {
+m = month2[u2] ; break;
+}
+}
+
+var daystr = day+ ' ' + m + ' ' + y ;
+var trtd = '<li class="car"><div class="thumb"><a href="'+posturl+'"><img width="170" height="215" class="alignnone" src="'+img[i]+'"/></a></div><p><a class="slider_title" href="'+posturl+'">'+posttitle+'</a></p></li>';
+
+document.write(trtd);
+j++;
+}
+document.write('</ul>');
 }
