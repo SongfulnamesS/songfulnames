27854e28a9ee58ba7c32367e56dde6ffdcff59cf
ISongfulnamesSI@gmail.com <ISongfulnamesSI@gmail.com@56615b9d-a27f-5bd6-95a2-5bceebee896c>
2014-04-24 21:54:51 +0000

    c

--- videosxd.js
+++ videosxd.js
@@ -269,7 +269,7 @@ function ultimosanime(json) {
         d = s.substr(b + 5, c - b - 5);
         if ((a != -1) && (b != -1) && (c != -1) && (d != "")) img[i] = d;
        
-      var trtd = '<li><a href="' + posturl + '">' + posttitle + '  <span>'+daystr+'</span>   </a></li>';
+      var trtd = '<li><a href="' + posturl + '">' + posttitle + ''+daystr+'</a></li>';
         document.write(trtd);
         j++
     }
