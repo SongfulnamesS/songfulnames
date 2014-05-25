6567accd536ff23773cf923544a937f849254833
ISongfulnamesSI@gmail.com <ISongfulnamesSI@gmail.com@56615b9d-a27f-5bd6-95a2-5bceebee896c>
2014-05-25 02:29:28 +0000

    f

--- loader.js
+++ loader.js
@@ -1,127 +1,107 @@
-﻿window.addEvent('domready',function(){
-
-/* search */
-
-var searchBox = $('search-box'), searchLoaded=false, searchFn = function() {
-
-/*
-
-We're lazyloading all of the search stuff.
-
-After all, why create elements, add listeners, etc. if the user never gets there?
-
-*/
-
-if(!searchLoaded) {
-
-searchLoaded = true; //set searchLoaded to "true"; no more loading!
-
-//build elements!
-
-var container = new Element('div',{ id: 'search-results' }).inject($('search-area'),'after');
-
-var wrapper = new Element('div',{
-
-styles: {
-
-position: 'relative'
-
+function viki(IDviki) {
+  var output = "<iframe src=\"http://zet.videosxd.org/repro-rc/viki?v=" + IDviki + "\" width=\"100%\" height=\"350\" frameborder=\"0\" scrolling=\"no\" scanned=\"yes\">";
+  output += "</iframe>";
+  document.write (output);
 }
 
-}).inject(container);
-
-new Element('div',{ id: 'search-results-pointer' }).inject(wrapper);
-
-var contentContainer = new Element('div',{ id: 'search-results-content' }).inject(wrapper);
-
-var closer = new Element('a', {
-
-href: 'javascript:;',
-
-text: 'Close',
-
-styles: {
-
-position: 'absolute', //position the "Close" link
-
-bottom: 35,
-
-right: 20
-
-},
-
-events: {
-
-click: function() {
-
-container.fade(0);
-
+function facebook(IDfacebook) {
+  var output = "<iframe src=\"http://zet.videosxd.org/repro-rc/face?v=" + IDfacebook + "\" width=\"100%\" height=\"350\" frameborder=\"0\" scrolling=\"no\">";
+  output += "</iframe>";
+  document.write (output);
 }
 
+function videobam(IDvideobam) {
+  var output = "<iframe src=\"http://zet.videosxd.org/repro-rc/bam?v=" + IDvideobam + "\" width=\"100%\" height=\"350\" frameborder=\"0\" scrolling=\"no\">";
+  output += "</iframe>";
+  document.write (output);
 }
 
-}).inject(wrapper);
-
-//google interaction
-
-var search = new google.search.WebSearch(),
-
-control = new google.search.SearchControl(),
-
-options = new google.search.DrawOptions();
-
-//set google options
-
-options.setDrawMode(google.search.SearchControl.DRAW_MODE_TABBED);
-
-options.setInput(searchBox);
-
-//set search options
-
-search.setUserDefinedClassSuffix('siteSearch');
-
-search.setSiteRestriction('davidwalsh.name');
-
-search.setLinkTarget(google.search.Search.LINK_TARGET_SELF);
-
-//set search controls
-
-control.addSearcher(search);
-
-control.draw(contentContainer,options);
-
-control.setNoResultsString('No results were found.');
-
-//add listeners to search box
-
-searchBox.addEvents({
-
-keyup: function(e) {
-
-if(searchBox.value && searchBox.value != searchBox.get('placeholder')) {
-
-container.fade(0.9);
-
-control.execute(searchBox.value);
-
+function twivideo(IDtwivideo) {
+  var output = "<iframe src=\"http://zet.videosxd.org/repro-rc/twi?v=" + IDtwivideo + "\" width=\"100%\" height=\"350\" frameborder=\"0\" scrolling=\"no\">";
+  output += "</iframe>";
+  document.write (output);
 }
 
-else {
-
-container.fade(0);
-
+function novamov(IDnovamov) {
+  var output = "<iframe title=\"novamov video player\"width=\"650\"height=\"350\"src=\"http://embed.novamov.com/embed.php?width=650&amp; height=350&amp; v=" + IDnovamov + "&amp; px=1\"frameborder=\"0\"allowfullscreen>";
+  output += "</iframe>";
+  document.write (output);
 }
 
+function nowvideo(IDnowvideo) {
+  var output = "<iframe src=\"http://embed.nowvideo.eu/embed.php?v=" + IDnowvideo + "&amp; width=650&amp; height=350\"frameborder=\"0\"scrolling=\"no\"allowtransparency=\"true\"width=\"650\"height=\"350\"scanned=\"yes\">";
+  output += "</iframe>";
+  document.write (output);
 }
 
-});
-
-searchBox.removeEvent('focus',searchFn);
-
+function dramafever(IDdramafever) {
+  var output = "<object classid=\"clsid:d27cdb6e-ae6d-11cf-96b8-444553540000\" width=\"650\" height=\"350\" align=\"middle\" id=\"dfPlayerEmbed\"><param name=\"movie\" value=\"http://imgdf-a.akamaihd.net/static/dfPlayer.swf\"/><param name=\"allowfullscreen\" value=\"true\"><param name=\"allowscriptaccess\" value=\"always\"><param name=\"allownetworking\" value=\"all\"><param name=\"flashvars\" value=\"sid=" + IDdramafever + "&url=http://token.dramafever.com&embed=embed\"><!--[if !IE]>--><object type=\"application/x-shockwave-flash\" data=\"http://imgdf-a.akamaihd.net/static/dfPlayer.swf\" width=\"650\" height=\"350\"><param name=\"movie\" value=\"http://imgdf-a.akamaihd.net/static/dfPlayer.swf\"/><param name=\"allowfullscreen\" value=\"true\"><param name=\"allowscriptaccess\" value=\"always\"><param name=\"allownetworking\" value=\"all\"><param name=\"flashvars\" value=\"sid=" + IDdramafever + "&url=http://token.dramafever.com&embed=embed\"><!--<![endif]--><a href=\"http://www.adobe.com/go/getflash\"><img src=\"http://www.adobe.com/images/shared/download_buttons/get_flash_player.gif\" alt=\"Get Adobe Flash player\"/></a><!--[if !IE]>--></object><!--<![endif]--></object>";
+  output += "</object>";
+  document.write (output);
 }
 
-};
+function leyendas(IDleyendas) {
+  var output = "<embed id='v_player_cctv' width='650' height='350' flashvars='videoId=" + IDleyendas + "&tai=spanish&configPath=http://js.player.cntv.cn/xml/config/outside.xml&widgetsConfig=&languageConfig=http://js.player.cntv.cn/xml/spanish/main.xml&hour24DataURL=&outsideChannelId=channelBugu&videoCenterId=232d2bad3d8c41d1bfac71f18586c6e1' allowscriptaccess='always' allowfullscreen='true' menu='false' quality='best' bgcolor='#000000' name='v_player_cctv' src='http://player.cntv.cn/standard/cntvOutSidePlayer.swf' type='application/x-shockwave-flash' lk_mediaid='lk_juiceapp_mediaPopup_1257416656250' lk_media='yes'>";
+  output += "</embed>";
+  document.write (output);
+}
 
-searchBox.addEvent('focus',searchFn);
+function premiun(IDpremiun) {
+ var output = "<embed class=\"player_conte\" src=\"https://plantillas-lex.googlecode.com/svn/trunk/reproductores/jkanime/player.swf\" width=\"650\" height=\"350\" allowscriptaccess=\"always\" allowfullscreen=\"true\" wmode=\"opaque\" flashvars=\"file=" + IDpremiun + "&amp;provider=video&amp;stretching=exactfit&amp;controlbar.position=over&amp;skin=https://plantillas-lex.googlecode.com/svn/trunk/reproductores/jkanime/ia.zip&amp;logo.file=http://1.bp.blogspot.com/-mPAQ7kykKj0/U1FjhNTfpkI/AAAAAAAACC8/gVULCYIde9s/s1600/logoeskina.png&amp;logo.position=top-right&amp;logo.link=http://songfulme.blogspot.com/&amp;logo.hide=false&amp;logo.timeout=5&amp;logo.out=0.4&amp;abouttext=Songful AnimeID anime de calidad&amp;aboutlink=#&amp;plugins=backstroke-1,timeslidertooltipplugin-1\">";
+ output += "</embed>";
+ document.write (output);
+}
 
-});
+function vervideos(IDvervideos) {
+ var output = "<embed type=\"application/x-shockwave-flash\" src=\"http://www.kenh88.com/player.swf\" width=\"650\" height=\"350\" style=\"undefined\" id=\"movie_player\" name=\"movie_player\" quality=\"high\" wmode=\"opaque\" allowfullscreen=\"true\" allowscriptaccess=\"always\" flashvars=\"plugins=http://www.kenh88.com/captions.swf,http://www.kenh88.com/plugins4/proxy.swf&amp;proxy.image=/images/phim-online-youtube-server.jpg&amp;proxy.link=" + IDvervideos + "&amp;captions.color=#FFCC00&amp;captions.fontFamily=Arial&amp;captions.fontSize=17&amp;captions.fontWeight=bold&amp;captions.state=true&amp;dock=true\">";
+  output += "</embed>";
+  document.write (output);
+}
+
+function vk(IDvk) {
+  var output = "<iframe title=\"VK video player Online HD\"width=\"650\"height=\"350\"src=\"http://vk.com/video_ext.php?oid=" + IDvk + "\"frameborder=\"0\"style=\"margin:0px; \"allowfullscreen>";
+  output += "</iframe>";
+  document.write (output);
+}
+
+function picasa(IDpicasa) {
+  var output = "<iframe src=\"http://zet.videosxd.org/repro-rc/picasa?v=" + IDpicasa + "\" width=\"100%\" height=\"350\" frameborder=\"0\" scrolling=\"no\" scanned=\"yes\">";
+  output += "</iframe>";
+  document.write (output);
+}
+
+function dailymotion(IDdailymotion) {
+  var output = "<iframe frameborder=\"0\" width=\"650\" height=\"350\" src=\"http://www.dailymotion.com/embed/video/" + IDdailymotion + "\">";
+  output += "</iframe>";
+  document.write (output);
+}
+
+function zippyshare(IDzippyshare) {
+  var output = "<iframe src=\"http://zet.videosxd.org/repro-rc/zipy?v=" + IDzippyshare + "\" width=\"650\" height=\"350\" frameborder=\"0\" scrolling=\"no\">";
+  output += "</iframe>";
+  document.write (output);
+}
+
+function shared(IDshared) {
+  var output = "<iframe src=\"http://zet.videosxd.org/repro-rc/shared?v=" + IDshared + "\" width=\"100%\" height=\"350\" frameborder=\"0\" scrolling=\"no\">";
+  output += "</iframe>";
+  document.write (output);
+}
+
+function youtube(IDyoutube) {
+  var output = "<iframe width=\"100%\" height=\"350\" src=\"//www.youtube.com/embed/" + IDyoutube + "\" frameborder=\"0\" allowfullscreen>";
+  output += "</iframe>";
+  document.write (output);
+}
+
+function vimple(IDvimple) {
+  var output = "<iframe src=\"http://player.vimple.ru/iframe/" + IDvimple + "\" width=\"100%\" height=\"340\" frameborder=\"0\" style=\"z-index:2147483647;\">";
+  output += "</iframe>";
+  document.write (output);
+}
+
+function vimeo(IDvimeo) {
+  var output = "<iframe src=\"//player.vimeo.com/video/" + IDvimeo + "?color=ff0179\" width=\"650\" height=\"350\" frameborder=\"0\" webkitallowfullscreen mozallowfullscreen allowfullscreen>";
+  output += "</iframe>";
+  document.write (output);
+}
