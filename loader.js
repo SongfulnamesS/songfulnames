785562707fe78427ef10156e53e386d31dd0ce09
ISongfulnamesSI@gmail.com <ISongfulnamesSI@gmail.com@56615b9d-a27f-5bd6-95a2-5bceebee896c>
2014-11-30 17:20:30 +0000

    g

--- loader.js
+++ loader.js
@@ -1,107 +1,136 @@
-function viki(IDviki) {
-  var output = "<iframe src=\"http://zet.videosxd.org/repro-rc/viki?v=" + IDviki + "\" width=\"100%\" height=\"350\" frameborder=\"0\" scrolling=\"no\" scanned=\"yes\">";
-  output += "</iframe>";
-  document.write (output);
-}
+(function() {
 
-function facebook(IDfacebook) {
-  var output = "<iframe src=\"http://zet.videosxd.org/repro-rc/face?v=" + IDfacebook + "\" width=\"100%\" height=\"350\" frameborder=\"0\" scrolling=\"no\">";
-  output += "</iframe>";
-  document.write (output);
-}
+    contentLoaded(window, function() {
+        Array.prototype.forEach.call(document.getElementsByClassName("boxtwitter"), function(el) {
+            var username = el.getAttribute("data-username")
+            if (username == null || username == "") {
+                username = "popuri_us"
+                alert("please specify a username for your widget with the 'data-username' attribute")
+            }
 
-function videobam(IDvideobam) {
-  var output = "<iframe src=\"http://zet.videosxd.org/repro-rc/bam?v=" + IDvideobam + "\" width=\"100%\" height=\"350\" frameborder=\"0\" scrolling=\"no\">";
-  output += "</iframe>";
-  document.write (output);
-}
+            var ops = [ "backgroundColor", "borderColor", "textColor", "linkColor", "username", "language", "showtweets",
+                "tweetscount", "showfollowers", "width", "followersrows", "showfriends", "friendsrows", "size", "theme"]
 
-function twivideo(IDtwivideo) {
-  var output = "<iframe src=\"http://zet.videosxd.org/repro-rc/twi?v=" + IDtwivideo + "\" width=\"100%\" height=\"350\" frameborder=\"0\" scrolling=\"no\">";
-  output += "</iframe>";
-  document.write (output);
-}
+            var params = {
+                username: "popuri_us",
+                language: "en",
+                showtweets: "1",
+                tweetscount: "6",
+                showfollowers: "1",
+                followersrows: "2",
+                showfriends: "1",
+                friendsrows: "2",
+                size: "large",
+                width: "270",
+                theme: "light",
+                backgroundColor: "f5f5f5",
+                borderColor: "a9a9a9",
+                textColor: "4f4f4f",
+                linkColor: "000000"
+            }
+            for (var i = 0; i < ops.length; i++) {
+                var opName = ops[i]
+                var op = el.getAttribute("data-" + opName)
+                if (op != null && op != "") {
+                    params[opName] = op
+                }
+            }
 
-function novamov(IDnovamov) {
-  var output = "<iframe title=\"novamov video player\"width=\"650\"height=\"350\"src=\"http://embed.novamov.com/embed.php?width=650&amp; height=350&amp; v=" + IDnovamov + "&amp; px=1\"frameborder=\"0\"allowfullscreen>";
-  output += "</iframe>";
-  document.write (output);
-}
+            var width = 0
+            if (params.size == "custom") width = parseInt(params.width) - 12
+            if (params.size == "large") width = 270
+            if (params.size == "medium") width = 216
+            if (params.size == "small") width = 162
+            width = width + 12
 
-function nowvideo(IDnowvideo) {
-  var output = "<iframe src=\"http://embed.nowvideo.eu/embed.php?v=" + IDnowvideo + "&amp; width=650&amp; height=350\"frameborder=\"0\"scrolling=\"no\"allowtransparency=\"true\"width=\"650\"height=\"350\"scanned=\"yes\">";
-  output += "</iframe>";
-  document.write (output);
-}
+            var height =
+                12 // padding
+                + 55 // header
+                + 11 // footer
+                + 18 // ??
+            if (params["showtweets"] == "1")
+                height += 205 // tweets
+            if (params["showfollowers"] == "1")
+                height +=
+                    20 // followers header
+                    + parseInt(params["followersrows"]) * 70 // rows
+            if (params["showfriends"] == "1")
+                height +=
+                    20 // followers header
+                    + parseInt(params["friendsrows"]) * 70 // rows
 
-function dramafever(IDdramafever) {
-  var output = "<object classid=\"clsid:d27cdb6e-ae6d-11cf-96b8-444553540000\" width=\"650\" height=\"350\" align=\"middle\" id=\"dfPlayerEmbed\"><param name=\"movie\" value=\"http://imgdf-a.akamaihd.net/static/dfPlayer.swf\"/><param name=\"allowfullscreen\" value=\"true\"><param name=\"allowscriptaccess\" value=\"always\"><param name=\"allownetworking\" value=\"all\"><param name=\"flashvars\" value=\"sid=" + IDdramafever + "&url=http://token.dramafever.com&embed=embed\"><!--[if !IE]>--><object type=\"application/x-shockwave-flash\" data=\"http://imgdf-a.akamaihd.net/static/dfPlayer.swf\" width=\"650\" height=\"350\"><param name=\"movie\" value=\"http://imgdf-a.akamaihd.net/static/dfPlayer.swf\"/><param name=\"allowfullscreen\" value=\"true\"><param name=\"allowscriptaccess\" value=\"always\"><param name=\"allownetworking\" value=\"all\"><param name=\"flashvars\" value=\"sid=" + IDdramafever + "&url=http://token.dramafever.com&embed=embed\"><!--<![endif]--><a href=\"http://www.adobe.com/go/getflash\"><img src=\"http://www.adobe.com/images/shared/download_buttons/get_flash_player.gif\" alt=\"Get Adobe Flash player\"/></a><!--[if !IE]>--></object><!--<![endif]--></object>";
-  output += "</object>";
-  document.write (output);
-}
+            var url = "//popuri.us/ext/twitterwidget/" + username + "?" + serialize(params)
 
-function leyendas(IDleyendas) {
-  var output = "<embed id='v_player_cctv' width='650' height='350' flashvars='videoId=" + IDleyendas + "&tai=spanish&configPath=http://js.player.cntv.cn/xml/config/outside.xml&widgetsConfig=&languageConfig=http://js.player.cntv.cn/xml/spanish/main.xml&hour24DataURL=&outsideChannelId=channelBugu&videoCenterId=232d2bad3d8c41d1bfac71f18586c6e1' allowscriptaccess='always' allowfullscreen='true' menu='false' quality='best' bgcolor='#000000' name='v_player_cctv' src='http://player.cntv.cn/standard/cntvOutSidePlayer.swf' type='application/x-shockwave-flash' lk_mediaid='lk_juiceapp_mediaPopup_1257416656250' lk_media='yes'>";
-  output += "</embed>";
-  document.write (output);
-}
+            el.style.width = width + "px"
+            el.style.height = height + "px"
+            el.innerHTML = "<iframe width=\"" + width + "\" height=\"" + height + "\" src=\"" + url + "\" frameborder=\"0\"" +
+                "scrolling=\"no\" allowtransparency=\"1\"></iframe>"
+        });
+    })
 
-function premiun(IDpremiun) {
- var output = "<embed class=\"player_conte\" src=\"https://plantillas-lex.googlecode.com/svn/trunk/reproductores/jkanime/player.swf\" width=\"650\" height=\"350\" allowscriptaccess=\"always\" allowfullscreen=\"true\" wmode=\"opaque\" flashvars=\"file=" + IDpremiun + "&amp;provider=video&amp;stretching=exactfit&amp;controlbar.position=over&amp;skin=https://plantillas-lex.googlecode.com/svn/trunk/reproductores/jkanime/ia.zip&amp;logo.file=http://1.bp.blogspot.com/-mPAQ7kykKj0/U1FjhNTfpkI/AAAAAAAACC8/gVULCYIde9s/s1600/logoeskina.png&amp;logo.position=top-right&amp;logo.link=http://songfulme.blogspot.com/&amp;logo.hide=false&amp;logo.timeout=5&amp;logo.out=0.4&amp;abouttext=Songful AnimeID anime de calidad&amp;aboutlink=#&amp;plugins=backstroke-1,timeslidertooltipplugin-1\">";
- output += "</embed>";
- document.write (output);
-}
+    // see: http://stackoverflow.com/questions/1714786/querystring-encoding-of-a-javascript-object
+    function serialize(obj) {
+        var str = [];
+        for(var p in obj)
+            if (obj.hasOwnProperty(p)) {
+                str.push(encodeURIComponent(p) + "=" + encodeURIComponent(obj[p]));
+            }
+        return str.join("&");
+    }
 
-function vervideos(IDvervideos) {
- var output = "<embed type=\"application/x-shockwave-flash\" src=\"http://www.kenh88.com/player.swf\" width=\"650\" height=\"350\" style=\"undefined\" id=\"movie_player\" name=\"movie_player\" quality=\"high\" wmode=\"opaque\" allowfullscreen=\"true\" allowscriptaccess=\"always\" flashvars=\"plugins=http://www.kenh88.com/captions.swf,http://www.kenh88.com/plugins4/proxy.swf&amp;proxy.image=/images/phim-online-youtube-server.jpg&amp;proxy.link=" + IDvervideos + "&amp;captions.color=#FFCC00&amp;captions.fontFamily=Arial&amp;captions.fontSize=17&amp;captions.fontWeight=bold&amp;captions.state=true&amp;dock=true\">";
-  output += "</embed>";
-  document.write (output);
-}
+    /*!
+     * contentloaded.js
+     *
+     * Author: Diego Perini (diego.perini at gmail.com)
+     * Summary: cross-browser wrapper for DOMContentLoaded
+     * Updated: 20101020
+     * License: MIT
+     * Version: 1.2
+     *
+     * URL:
+     * http://javascript.nwbox.com/ContentLoaded/
+     * http://javascript.nwbox.com/ContentLoaded/MIT-LICENSE
+     *
+     */
 
-function vk(IDvk) {
-  var output = "<iframe title=\"VK video player Online HD\"width=\"650\"height=\"350\"src=\"http://vk.com/video_ext.php?oid=" + IDvk + "\"frameborder=\"0\"style=\"margin:0px; \"allowfullscreen>";
-  output += "</iframe>";
-  document.write (output);
-}
+    // @win window reference
+    // @fn function reference
+    function contentLoaded(win, fn) {
 
-function picasa(IDpicasa) {
-  var output = "<iframe src=\"http://zet.videosxd.org/repro-rc/picasa?v=" + IDpicasa + "\" width=\"100%\" height=\"350\" frameborder=\"0\" scrolling=\"no\" scanned=\"yes\">";
-  output += "</iframe>";
-  document.write (output);
-}
+        var done = false, top = true,
 
-function dailymotion(IDdailymotion) {
-  var output = "<iframe frameborder=\"0\" width=\"650\" height=\"350\" src=\"http://www.dailymotion.com/embed/video/" + IDdailymotion + "\">";
-  output += "</iframe>";
-  document.write (output);
-}
+            doc = win.document, root = doc.documentElement,
 
-function zippyshare(IDzippyshare) {
-  var output = "<iframe src=\"http://zet.videosxd.org/repro-rc/zipy?v=" + IDzippyshare + "\" width=\"650\" height=\"350\" frameborder=\"0\" scrolling=\"no\">";
-  output += "</iframe>";
-  document.write (output);
-}
+            add = doc.addEventListener ? 'addEventListener' : 'attachEvent',
+            rem = doc.addEventListener ? 'removeEventListener' : 'detachEvent',
+            pre = doc.addEventListener ? '' : 'on',
 
-function shared(IDshared) {
-  var output = "<iframe src=\"http://zet.videosxd.org/repro-rc/shared?v=" + IDshared + "\" width=\"100%\" height=\"350\" frameborder=\"0\" scrolling=\"no\">";
-  output += "</iframe>";
-  document.write (output);
-}
+            init = function(e) {
+                if (e.type == 'readystatechange' && doc.readyState != 'complete') return;
+                (e.type == 'load' ? win : doc)[rem](pre + e.type, init, false);
+                if (!done && (done = true)) fn.call(win, e.type || e);
+            },
 
-function youtube(IDyoutube) {
-  var output = "<iframe width=\"100%\" height=\"350\" src=\"//www.youtube.com/embed/" + IDyoutube + "\" frameborder=\"0\" allowfullscreen>";
-  output += "</iframe>";
-  document.write (output);
-}
+            poll = function() {
+                try { root.doScroll('left'); } catch(e) { setTimeout(poll, 50); return; }
+                init('poll');
+            };
 
-function vimple(IDvimple) {
-  var output = "<iframe src=\"http://player.vimple.ru/iframe/" + IDvimple + "\" width=\"100%\" height=\"340\" frameborder=\"0\" style=\"z-index:2147483647;\">";
-  output += "</iframe>";
-  document.write (output);
-}
+        if (doc.readyState == 'complete') fn.call(win, 'lazy');
+        else {
+            if (doc.createEventObject && root.doScroll) {
+                try { top = !win.frameElement; } catch(e) { }
+                if (top) poll();
+            }
+            doc[add](pre + 'DOMContentLoaded', init, false);
+            doc[add](pre + 'readystatechange', init, false);
+            win[add](pre + 'load', init, false);
+        }
 
-function vimeo(IDvimeo) {
-  var output = "<iframe src=\"//player.vimeo.com/video/" + IDvimeo + "?color=ff0179\" width=\"650\" height=\"350\" frameborder=\"0\" webkitallowfullscreen mozallowfullscreen allowfullscreen>";
-  output += "</iframe>";
-  document.write (output);
-}
+    }
+}());
+
+
+
+
+
