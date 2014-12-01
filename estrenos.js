454dffeb0f4bb71d3b8a52b81d6ffef066bdd578
ISongfulnamesSI@gmail.com <ISongfulnamesSI@gmail.com@56615b9d-a27f-5bd6-95a2-5bceebee896c>
2014-12-01 21:55:27 +0000

    d

--- estrenos.js
+++ estrenos.js
@@ -120,7 +120,7 @@ function gratisme(json) {
             }
         }
         var daystr = day + ' ' + m + ' ' + y;
-      var trtd = '<li><a class="rated_avatar" title="'+ posttitle +'" href="' + posturl + '"><img src="' + img[i] + '" width="47" height="47" border="0" alt="' + posttitle + '"/></a><a class="rated_title" href="' + posturl + '">' + posttitle + '</a><div class="rated_stars"><span>'+daystr+'</span><img src="http://4.bp.blogspot.com/-iLWPFywYVgg/U0a2zVfFqeI/AAAAAAAACA0/wEp4CmjnBLc/s1600/estrella.png" style="height:11px; vertical-align:middle;" title="Progamacion Jkanime" alt="Progamacion Jkanime"></div><a class="rated_more" href=' + posturl + '><img style="vertical-align:middle;" src="http://3.bp.blogspot.com/-Tb6i2OrgUrs/U0a21laJahI/AAAAAAAACA8/21CmqxspRYw/s1600/verr.png" alt="Ver Anime"></a></li>';
+      var trtd = '<li><a class="rated_avatar" title="'+ posttitle +'" href="' + posturl + '"><img src="' + img[i] + '" width="47" height="47" border="0" alt="' + posttitle + '"/></a><a class="rated_title" href="' + posturl + '">' + posttitle + '</a><div class="rated_stars"><span>'+daystr+'</span><img src="http://4.bp.blogspot.com/-iLWPFywYVgg/U0a2zVfFqeI/AAAAAAAACA0/wEp4CmjnBLc/s1600/estrella.png" style="height:11px; vertical-align:middle;" title="Progamacion Estrenos" alt="Progamacion Jkanime"></div><a class="rated_more" href=' + posturl + '><img style="vertical-align:middle;" src="http://3.bp.blogspot.com/-Tb6i2OrgUrs/U0a21laJahI/AAAAAAAACA8/21CmqxspRYw/s1600/verr.png" alt="Ver Anime"></a></li>';
         document.write(trtd);
         j++
     }
