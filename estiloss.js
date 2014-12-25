ffb2f58db2b8b5d5624a1169931d0e3d58d22736
ISongfulnamesSI@gmail.com <ISongfulnamesSI@gmail.com@56615b9d-a27f-5bd6-95a2-5bceebee896c>
2014-12-25 22:09:35 +0000

    trsa

--- estiloss.js
+++ estiloss.js
@@ -169,7 +169,7 @@ function anime(json) {
             }
         }
         var daystr = day + ' ' + m + ' ' + y;
-      var trtd = '<div class="home_portada_bg"><a class="big_portada_title" title="'+ posttitle +'" href="' + posturl + '">' + posttitle + '</a><a class="portada_big" title="'+ posttitle +'" href="' + posturl + '"><img src="' + img[i] + '" width="151" height="250" alt="' + posttitle + '"/></a></div>';
+      var trtd = '<div class="home_portada_bg"><a class="big_portada_title" title="'+ posttitle +'" href="' + posturl + '">' + posttitle + '</a><a class="portada_big" title="'+ posttitle +'" href="' + posturl + '"><img src="' + img[i] + '" width="151" height="250" alt="' + posttitle + '"/><center><script>compartir()</script></center></a></div>';
         document.write(trtd);
         j++
     }
