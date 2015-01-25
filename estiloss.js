a6ed5200067d1ad9cbb4ac34a002c544d86509e0
ISongfulnamesSI@gmail.com <ISongfulnamesSI@gmail.com@56615b9d-a27f-5bd6-95a2-5bceebee896c>
2015-01-25 17:13:07 +0000

    tyuo

--- estiloss.js
+++ estiloss.js
@@ -515,7 +515,7 @@ function romancitas(json) {
         }
         var daystr = day + ' ' + m + ' ' + y;
         pcm = '<a href="' + posturl + '">' + pcm + ' comments</a>';
-      var trtd = '<div class="lstsradd"><div class="snplblkn"></div><div class="animedt">' + posttitle + '</div><a class="tooltip" title="'+ posttitle +'" href="' + posturl + '"><div class="lstanmadd-lston"></div><img class="imglstsr" src="' + img[i] + '" width="154" border="0" alt="' + posttitle + '"/></a></div>';
+      var trtd = '<div class="lstsradd"><div class="snplblkn"></div><div class="animedt">' + posttitle + '</div><a class="tooltip" title="'+ posttitle +'" href="' + posturl + '"><div class="lstanmadd-lston"></div><img class="imglstsr" src="' + img[i] + '" width="165" border="0" alt="' + posttitle + '"/></a></div>';
         document.write(trtd);
         j++
     }
