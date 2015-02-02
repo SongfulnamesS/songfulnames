d8d2afe3520d3c548b7c779d30480c88864f6bb3
ISongfulnamesSI@gmail.com <ISongfulnamesSI@gmail.com@56615b9d-a27f-5bd6-95a2-5bceebee896c>
2015-02-02 03:15:16 +0000

    lunas

--- estiloss.js
+++ estiloss.js
@@ -66,7 +66,7 @@ function topanime(json) {
             }
         }
         var daystr = day + ' ' + m + ' ' + y;
-      var trtd = '<div class="loltopanime"><div class="loltopimg"><a title="'+ posttitle +'" href="' + posturl + '"><img src="' + img[i] + '" width="166" border="0" alt="' + posttitle + '"/></a></div><div class="topanimedt"><span>'+numerar+' </span><a href="' + posturl + '">' + posttitle + '<a class="rated_more" href=' + posturl + '><img src="http://cdn.jkanime.net/assets/images/like.png" style="vertical-align:middle;"></a></a></div></div>';
+      var trtd = '<div class="loltopanime"><div class="loltopimg"><a title="'+ posttitle +'" href="' + posturl + '"><img src="' + img[i] + '" width="166" border="0" alt="' + posttitle + '"/></a></div><div class="topanimedt"><span>'+numerar+' </span><a href="' + posturl + '">' + posttitle + '<a class="manito" href=' + posturl + '><img src="http://cdn.jkanime.net/assets/images/like.png" style="vertical-align:middle;float:right;"></a></a></div></div>';
         document.write(trtd);
         j++
     }
